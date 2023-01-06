519dfec10972ffc2c167d6cb9c52803cfdddae31
eyurkin <ave2008@gmail.com>
2023-01-06 18:22:42 +0300

    Update ShareItGateway.java

--- gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
+++ gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
@@ -3,8 +3,6 @@ package ru.practicum.shareit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.ZoneId;
-
 @SpringBootApplication
 public class ShareItGateway {
     public static void main(String[] args) {
